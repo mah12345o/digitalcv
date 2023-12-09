05ce0b763511ce8b0eb6ad54f648064241a5e780
Maheshsuthar <107390064+mah12345o@users.noreply.github.com>
2023-12-09 17:11:36 +0530

    first step

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <div className='row'>
+       <div className="main-section p-4 col-12 col-md-8 col-lg-8 m-auto h-100 row my-5">
+      <div className=' col-md-8 col-lg-8 '>
+        <h2>mahesh kumar</h2>
+      </div>
+      {/* Gota, Ahmedabad 382481, 9429230712, maheshsuthar083@gmail.com */}
+      <div className=' font-1 addres-section  col-md-4 col-lg-4 '>
+        <div> Gota , Ahmedabad 382481</div>
+        <div>9429230712 </div>
+        <div>maheshsuthar083@gmail.com</div>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
