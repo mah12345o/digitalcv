661dd6d57bcd1527cc9badcff61713a521880927
Maheshsuthar <107390064+mah12345o@users.noreply.github.com>
2024-01-07 23:59:40 +0530

    All Content Added.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 // import userImg from './mkpng.png';
 
+
 function App() {
   return (
     <div className='row '>
@@ -17,7 +18,7 @@ function App() {
           <div>maheshsuthar083@gmail.com</div>
         </div>
         <div className=' col-md-8 col-lg-8 '>
-          <h6>EXPERIENCE
+          <h6>Exprience
           </h6>
           <div className='mb-3'>
             <h4>CloseDigit LLP
@@ -36,9 +37,10 @@ function App() {
             </div>
           </div>
         </div>
+
         <div className='font-1 addres-section  col-md-4 col-lg-4 '>
-          <div className='mb-3'> 
-            <h6>SKILLS
+          <div className='mb-3'>
+            <h6>Skills
             </h6>
             <div>Bootstrap , Mui , React JS , Redux ,
               ,Chrome Dev Tools ,
@@ -48,14 +50,72 @@ function App() {
             </div>
           </div>
           <div>
-            <h6>PROJECTS
+            <h6>Projects
             </h6>
             <div>https://triviamaker.com/.
               https://triviamaker.ai
               https://react.surveyslack.com/
             </div>
           </div>
+          
         </div>
+        
+        <div className=' col-md-8 col-lg-8 '>
+          <h6>Education
+          </h6>
+          <div className='mb-3'>
+            <h4>The Maharaja Sayaji Rao Univerisity Of Baroda
+            </h4>
+            <div> Vadodara(Gujarat),
+            </div>
+            <div>Graduation(B.sc.) 2016 - 202</div>
+            <div>Percentage-51</div>
+          </div>
+
+          <div>
+            <h4>Pink Model Senior Sec. School , Jalore
+              (Rajasthan)
+            </h4>
+            <div>Board-Rajasthan board of Secondary Education </div>
+            <div>12th Science(PCB)
+            </div>
+            <div>2016,
+              Percentage-73
+            </div>
+          </div>
+        </div>
+        <div className='font-1 addres-section  col-md-4 col-lg-4 '>
+          <div className='mb-3'>
+            <h6>Portfolio
+            </h6>
+            <div>Bootstrap , Mui , React JS , Redux ,
+              ,Chrome Dev Tools ,
+              Responsive Design , REST API ,
+              Testing & Debugging ,
+              [ Node ,Express ,Mongo ] Fresher
+            </div>
+          </div>
+          <div>
+            <h6>Projects
+            </h6>
+            <div>https://triviamaker.com/.
+              https://triviamaker.ai
+              https://react.surveyslack.com/
+            </div>
+          </div>
+          
+        </div>
+        <div className=' col-md-8 col-lg-8 '>
+          <h6>Profiles
+
+          </h6>
+          <div className='mb-3'>
+            <h6>https://www.linkedin.com/in/mahesh-kumar-160a10165/
+            </h6>
+
+          </div>
+        </div>
+        
       </div>
 
       {/* <div className=' font-1 addres-section  col-md-4 col-lg-4 '> */}
